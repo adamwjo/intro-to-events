18ff6d403fbba92106b0cccdefe195a67e5d5362
Adam Johnson <adamwjo@gmail.com>
2021-01-12 11:06:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addBubbleDelete(){
+//     document.querySelector('#table').addEventListener('click', (event) => {
+//         console.log(event.target.nodeName);
+//         if(event.target.nodeName === 'IMG'){
+//             event.target.remove()
+//         }
+        
+//     })
+// }
+
+
+// function getRow() {
+//     document.querySelectorAll('tr').forEach((row) => {
+//         row.addEventListener('click', (event) => {
+//             console.log(event.currentTarget.innerText);
+//         })
+//     })
+// }
